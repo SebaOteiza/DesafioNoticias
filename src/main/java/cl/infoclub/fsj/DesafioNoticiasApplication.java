76ec1ffec3d084ae3ac628af2948439263d7ca8b
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2022-11-09 21:17:17 -0300

    creando el proyecto

---
+++ src/main/java/cl/infoclub/fsj/DesafioNoticiasApplication.java
@@ -0,0 +1,13 @@
+package cl.infoclub.fsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNoticiasApplication.class, args);
+	}
+
+}
